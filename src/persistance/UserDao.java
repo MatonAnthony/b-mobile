b81a3648ec4295ce100b7ea7f18f1bade48986c6
Jonathan Samelson <jonathan@samelson.info>
2016-02-24 19:16:52 +0000

    Ajout des méthodes à l'interface userDao

--- src/persistance/UserDao.java
+++ src/persistance/UserDao.java
@@ -2,4 +2,12 @@ package persistance;
 
 public interface UserDao {
 
+  boolean createUser();
+
+  void read();
+
+  boolean update();
+
+  boolean delete();
+
 }
--- src/persistance/UserDaoImpl.java
+++ src/persistance/UserDaoImpl.java
@@ -2,4 +2,28 @@ package persistance;
 
 class UserDaoImpl implements UserDao {
 
+  @Override
+  public boolean createUser() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void read() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean update() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean delete() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
 }
