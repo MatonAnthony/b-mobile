d879de1e146b1253455c19378bb1e74844d546e9
Kamil Arszagi <kamil@arszagi.eu>
2016-03-01 14:33:11 +0000

    mise a jour data.prod.sql - remplacement des id hard code par Default car les id sont en 'SERIAL' ds la db

--- sql/data_prod.sql
+++ sql/data_prod.sql
@@ -9,10 +9,10 @@ TRUNCATE TABLE bmobile.partenaires CASCADE ;
 TRUNCATE TABLE bmobile.programmes CASCADE ;
 
 INSERT INTO bmobile.utilisateurs (id_utilisateur, pseudo, mdp, nom, prenom, email, droits)
-VALUES (1, 'kamil', 'kamil123', 'Arszagi vel Harszagi', 'Kamil', 'kamil.arszagi@student.vinci.be','ETUDIANT');
+VALUES (DEFAULT, 'kamil', 'kamil123', 'Arszagi vel Harszagi', 'Kamil', 'kamil.arszagi@student.vinci.be','ETUDIANT');
 
 INSERT INTO bmobile.utilisateurs (id_utilisateur, pseudo, mdp, nom, prenom, email, droits)
-VALUES (2, 'martin', 'martin123', 'Techy', 'Martin', 'martin.techy@student.vinci.be','ETUDIANT');
+VALUES (DEFAULT, 'martin', 'martin123', 'Techy', 'Martin', 'martin.techy@student.vinci.be','ETUDIANT');
 
-INSERT INTO bmobile.utilisateurs (pseudo, mdp, nom, prenom, email, droits)
-VALUES ('prof', 'prof123', 'ProfNom', 'ProfPrenom', 'prof@vinci.be','PROF');
+INSERT INTO bmobile.utilisateurs (id_utilisateur, pseudo, mdp, nom, prenom, email, droits)
+VALUES (DEFAULT, 'prof', 'prof123', 'ProfNom', 'ProfPrenom', 'prof@vinci.be','PROF');
