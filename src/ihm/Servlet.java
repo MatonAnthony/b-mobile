9ff0bb61aa7125ee60c76d31f152d267043bf7ee
Martin Techy <martin_martin1993@hotmail.com>
2016-03-16 18:53:53 +0000

    Corrections d'erreurs lors de l'authentification

--- src/ihm/Servlet.java
+++ src/ihm/Servlet.java
@@ -13,7 +13,11 @@ import com.owlike.genson.reflect.VisibilityFilter;
 
 import org.eclipse.jetty.http.HttpStatus;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
@@ -29,204 +33,214 @@ import javax.servlet.http.HttpSession;
 
 public class Servlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private static final String SECRET =
-        "LICORNEkjcajn edea zfalzenf  faezfbalzbflf5f5eaz45 546 a4f5 af46 aezPONEY";
+  private static final String SECRET =
+      "LICORNEkjcajn edea zfalzenf  faezfbalzbflf5f5eaz45 546 a4f5 af46 aezPONEY";
 
-    private static final String CONTENT_TYPE = "text/html; charset=utf-8";
-    private static final String HTML_BODY_PAGES_PATH = "www/assets/pages/";
+  private static final String CONTENT_TYPE = "text/html; charset=utf-8";
+  private static final String HTML_BODY_PAGES_PATH = "www/assets/pages/";
 
-    private transient UserUcController userUcc = null;
-    private transient BizzFactory bizzFactory = null;
+  private static final String KEY_USERNAME = "username";
+  private static final String KEY_PERMISSIONS = "permissions";
+  private static final String KEY_ID = "id";
 
-    private transient Genson genson =
-        new GensonBuilder().useFields(true, VisibilityFilter.PRIVATE).useMethods(false)
-            .exclude("password").create();
+  private transient UserUcController userUcc = null;
+  private transient BizzFactory bizzFactory = null;
 
-    public Servlet(UserUcController userUcc, BizzFactory bizzFactory) {
-        this.userUcc = userUcc;
-        this.bizzFactory = bizzFactory;
+  private transient Genson genson = new GensonBuilder().useFields(true, VisibilityFilter.PRIVATE)
+      .useMethods(false).exclude("password").create();
+
+  public Servlet(UserUcController userUcc, BizzFactory bizzFactory) {
+    this.userUcc = userUcc;
+    this.bizzFactory = bizzFactory;
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    resp.setContentType(CONTENT_TYPE);
+    PrintWriter out = resp.getWriter();
+    out.println(htmlToString("www/assets/header.html"));
+    out.println(htmlToString("www/assets/logo.html"));
+    out.println(htmlToString("www/assets/navBarStudent.html"));
+    out.println(htmlToString("www/assets/navBarTeacher.html"));
+    File folder = new File(HTML_BODY_PAGES_PATH);
+    File[] listOfFiles = folder.listFiles();
+    for (File file : listOfFiles) {
+      if (file.isFile()) {
+        out.println(htmlToString(HTML_BODY_PAGES_PATH + file.getName()));
+      }
     }
+    out.println(htmlToString("www/assets/footer.html"));
 
-    @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException {
+  }
 
-        resp.setContentType(CONTENT_TYPE);
-        PrintWriter out = resp.getWriter();
-        out.println(htmlToString("www/assets/header.html"));
-        out.println(htmlToString("www/assets/logo.html"));
-        out.println(htmlToString("www/assets/navBarStudent.html"));
-        out.println(htmlToString("www/assets/navBarTeacher.html"));
-        File folder = new File(HTML_BODY_PAGES_PATH);
-        File[] listOfFiles = folder.listFiles();
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                out.println(htmlToString(HTML_BODY_PAGES_PATH + file.getName()));
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    try {
+      String action = req.getParameter("action");
+      HttpSession session = req.getSession();
+
+      switch (action) {
+
+        case "login":
+          String username = req.getParameter("username");
+          String password = req.getParameter("password");
+
+          UserDto userDtoRecept = userUcc.login(username, password);
+
+          if (userDtoRecept == null) {
+            resp.setStatus(HttpStatus.FORBIDDEN_403);
+          } else {
+            session.setAttribute("username", userDtoRecept.getPseudo());
+            session.setAttribute("permissions", userDtoRecept.getPermissions());
+            createJwtCookie(resp, userDtoRecept);
+            resp.setStatus(HttpStatus.ACCEPTED_202);
+            resp.getWriter().println(dtoToJson(userDtoRecept));
+
+          }
+          break;
+
+        case "authenticate":
+          resp.setStatus(HttpStatus.ACCEPTED_202);
+          UserDto userDto = bizzFactory.getUserDto();
+          if (null != session.getAttribute(KEY_USERNAME)) {
+            userDto.setPermissions("" + session.getAttribute(KEY_ID));
+            userDto.setPermissions("" + session.getAttribute(KEY_PERMISSIONS));
+            userDto.setPseudo("" + session.getAttribute(KEY_USERNAME));
+            createJwtCookie(resp, userDto);
+            resp.getWriter().println(dtoToJson(userDto));
+
+          } else {
+            if (readJwtCookie(req)) {
+              userDto.setPermissions("" + session.getAttribute(KEY_ID));
+              userDto.setPermissions("" + session.getAttribute(KEY_PERMISSIONS));
+              userDto.setPseudo("" + session.getAttribute(KEY_USERNAME));
+              resp.getWriter().println(dtoToJson(userDto));
+            } else {
+              resp.setStatus(HttpStatus.FORBIDDEN_403);
             }
+          }
+          break;
+
+        case "register":
+          UserDto userdto = bizzFactory.getUserDto();
+
+          userdto.setPseudo(req.getParameter("username"));
+          userdto.setPassword(req.getParameter("password"));
+          userdto.setName(req.getParameter("name"));
+          userdto.setFirstname(req.getParameter("firstname"));
+          userdto.setEmail(req.getParameter("email"));
+          String confirmation = req.getParameter("confirmation");
+
+          userDtoRecept = userUcc.register(userdto, confirmation);
+          if (userDtoRecept == null) {
+            resp.setStatus(HttpStatus.FORBIDDEN_403);
+          } else {
+            session.setAttribute(KEY_ID, userDtoRecept.getId());
+            session.setAttribute(KEY_USERNAME, userDtoRecept.getPseudo());
+            session.setAttribute(KEY_PERMISSIONS, userDtoRecept.getPermissions());
+            createJwtCookie(resp, userDtoRecept);
+            resp.setStatus(HttpStatus.ACCEPTED_202);
+            resp.getWriter().println(dtoToJson(userDtoRecept));
+          }
+
+          break;
+
+        case "confirmedMobility":
+          // Appel de fonction
+          break;
+
+        case "editProfile":
+
+          break;
+        default:
+          resp.setStatus(HttpStatus.BAD_REQUEST_400);
+      }
+
+    } catch (Exception exc) {
+      exc.printStackTrace();
+      resp.setStatus(HttpStatus.INTERNAL_SERVER_ERROR_500);
+      resp.getWriter().println(exc.getMessage());
+      resp.flushBuffer();
+    }
+
+  }
+
+  /**
+   * Lit le cookie JWT afin de verifier si l'utilisateur est authentifie.
+   *
+   * @param req La requete envoyee par la page
+   * @return true si l'utilisateur etait authentifie via JWT. false si il n'était pas authentifie.
+   */
+  private boolean readJwtCookie(HttpServletRequest req) {
+    Map<String, Object> decodedPayload;
+    try {
+      Cookie[] cookies = req.getCookies();
+      String token = null;
+      for (Cookie c : cookies) {
+        if ("user".equals(c.getName())) {
+          token = c.getValue();
         }
-        out.println(htmlToString("www/assets/footer.html"));
-
+      }
+      decodedPayload = new JWTVerifier(SECRET).verify(token);
+    } catch (InvalidKeyException | NoSuchAlgorithmException | IllegalStateException
+        | SignatureException | IOException | JWTVerifyException | NullPointerException exc) {
+      return false;
     }
+    req.getSession().setAttribute(KEY_ID, decodedPayload.get(KEY_ID));
+    req.getSession().setAttribute(KEY_USERNAME, decodedPayload.get(KEY_USERNAME));
+    req.getSession().setAttribute(KEY_PERMISSIONS, decodedPayload.get(KEY_PERMISSIONS));
+    return true;
+  }
 
-    @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException {
-        try {
-            String action = req.getParameter("action");
-            HttpSession session = req.getSession();
+  /**
+   * Cree un cookie avec un token JWT afin de ne pas perdre l'authentification d'un utilisateur.
+   *
+   * @param resp La reponse qui serra renvoyee par le serveur.
+   * @param login Le pseudo de l'utilisateur.
+   */
+  private void createJwtCookie(HttpServletResponse resp, UserDto userDto) {
 
-            switch (action) {
+    Map<String, Object> claims = new HashMap<String, Object>();
+    claims.put(KEY_USERNAME, userDto.getPseudo());
+    claims.put(KEY_PERMISSIONS, userDto.getPermissions());
+    claims.put(KEY_ID, userDto.getId());
 
-                case "login":
-                    String username = req.getParameter("username");
-                    String password = req.getParameter("password");
+    String token = new JWTSigner(SECRET).sign(claims);
 
-                    UserDto userDtoRecept = userUcc.login(username, password);
+    Cookie cookie = new Cookie("user", token);
+    cookie.setPath("/");
+    cookie.setMaxAge(60 * 60 * 24 * 365);
+    resp.addCookie(cookie);
+  }
 
-                    if (userDtoRecept == null) {
-                        resp.setStatus(HttpStatus.FORBIDDEN_403);
-                    } else {
-                        session.setAttribute("username", userDtoRecept.getPseudo());
-                        createJwtCookie(resp, userDtoRecept);
-                        resp.setStatus(HttpStatus.ACCEPTED_202);
-                        resp.getWriter().println(dtoToJson(userDtoRecept));
+  private String dtoToJson(UserDto dto) {
+    return genson.serialize(dto);
+  }
 
-                    }
-                    break;
-
-                case "authenticate":
-                    resp.setStatus(HttpStatus.ACCEPTED_202);
-                    UserDto userDto = bizzFactory.getUserDto();
-                    if (null != session.getAttribute("username")) {
-                        userDto.setPermissions("" + session.getAttribute("rights"));
-                        userDto.setPseudo("" + session.getAttribute("username"));
-                        createJwtCookie(resp, userDto);
-                        resp.getWriter().println(dtoToJson(userDto));
-
-                    } else {
-                        if (readJwtCookie(req)) {
-                            userDto.setPermissions("" + session.getAttribute("rights"));
-                            userDto.setPseudo("" + session.getAttribute("username"));
-                            resp.getWriter().println(dtoToJson(userDto));
-                        } else {
-                            resp.setStatus(HttpStatus.FORBIDDEN_403);
-                        }
-                    }
-                    break;
-
-                case "register":
-                    UserDto userdto = bizzFactory.getUserDto();
-
-                    userdto.setPseudo(req.getParameter("username"));
-                    userdto.setPassword(req.getParameter("password"));
-                    userdto.setName(req.getParameter("name"));
-                    userdto.setFirstname(req.getParameter("firstname"));
-                    userdto.setEmail(req.getParameter("email"));
-                    String confirmation = req.getParameter("confirmation");
-
-                    userDtoRecept = userUcc.register(userdto, confirmation);
-                    if (userDtoRecept == null) {
-                        resp.setStatus(HttpStatus.FORBIDDEN_403);
-                    } else {
-                        session.setAttribute("username", userDtoRecept.getPseudo());
-                        createJwtCookie(resp, userDtoRecept);
-                        resp.setStatus(HttpStatus.ACCEPTED_202);
-                        resp.getWriter().println(dtoToJson(userDtoRecept));
-                    }
-
-                    break;
-
-                case "confirmedMobility":
-                    // Appel de fonction
-                    break;
-
-                case "editProfile":
-
-                    break;
-                default:
-                    resp.setStatus(HttpStatus.BAD_REQUEST_400);
-            }
-
-        } catch (Exception exc) {
-            exc.printStackTrace();
-            resp.setStatus(HttpStatus.INTERNAL_SERVER_ERROR_500);
-            resp.getWriter().println(exc.getMessage());
-            resp.flushBuffer();
-        }
-
+  /**
+   * Extract html code from html file in an string
+   *
+   * @param file path of html file
+   * @return String
+   */
+  private String htmlToString(String file) {
+    StringBuilder contentBuilder = new StringBuilder();
+    try {
+      BufferedReader in = new BufferedReader(new FileReader(file));
+      String str;
+      while ((str = in.readLine()) != null) {
+        contentBuilder.append(str);
+      }
+      in.close();
+    } catch (IOException exc) {
+      exc.printStackTrace();
     }
-
-    /**
-     * Lit le cookie JWT afin de verifier si l'utilisateur est authentifie.
-     *
-     * @param req La requete envoyee par la page
-     * @return true si l'utilisateur etait authentifie via JWT. false si il n'était pas authentifie.
-     */
-    private boolean readJwtCookie(HttpServletRequest req) {
-        Map<String, Object> decodedPayload;
-        try {
-            Cookie[] cookies = req.getCookies();
-            String token = null;
-            for (Cookie c : cookies) {
-                if ("user".equals(c.getName())) {
-                    token = c.getValue();
-                }
-            }
-            decodedPayload = new JWTVerifier(SECRET).verify(token);
-        } catch (InvalidKeyException | NoSuchAlgorithmException | IllegalStateException
-            | SignatureException | IOException | JWTVerifyException | NullPointerException exc) {
-            return false;
-        }
-
-        req.getSession().setAttribute("username", decodedPayload.get("username"));
-        req.getSession().setAttribute("rights", decodedPayload.get("rights"));
-        return true;
-    }
-
-    /**
-     * Cree un cookie avec un token JWT afin de ne pas perdre l'authentification d'un utilisateur.
-     *
-     * @param resp  La reponse qui serra renvoyee par le serveur.
-     * @param login Le pseudo de l'utilisateur.
-     */
-    private void createJwtCookie(HttpServletResponse resp, UserDto userDto) {
-
-        Map<String, Object> claims = new HashMap<String, Object>();
-        claims.put("username", userDto.getPseudo());
-        claims.put("rights", userDto.getPermissions());
-        claims.put("id", "1");
-
-        String token = new JWTSigner(SECRET).sign(claims);
-
-        Cookie cookie = new Cookie("user", token);
-        cookie.setPath("/");
-        cookie.setMaxAge(60 * 60 * 24 * 365);
-        resp.addCookie(cookie);
-    }
-
-    private String dtoToJson(UserDto dto) {
-        return genson.serialize(dto);
-    }
-
-    /**
-     * Extract html code from html file in an string
-     *
-     * @param file path of html file
-     * @return String
-     */
-    private String htmlToString(String file) {
-        StringBuilder contentBuilder = new StringBuilder();
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(file));
-            String str;
-            while ((str = in.readLine()) != null) {
-                contentBuilder.append(str);
-            }
-            in.close();
-        } catch (IOException exc) {
-            exc.printStackTrace();
-        }
-        return contentBuilder.toString();
-    }
+    return contentBuilder.toString();
+  }
 
 }
