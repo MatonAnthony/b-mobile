7d645b5acf0c907b81a6b65be0cc46661d1cb093
Anthony Maton <me@anthony-maton.me>
2016-03-19 10:21:06 +0000

    Add a file with a Json to CSV function

---
+++ www/assets/js/csv.js
@@ -0,0 +1,28 @@
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    var CSV = '';
+    //Set Report title in first row or line
+    CSV += ReportTitle + '\r\n\n';
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = '';
+        for (var index in arrData[0]) {
+            row += index + ',';
+        }
+        row = row.slice(0, -1);
+        CSV += row + '\r\n';
+    }
+    for (var i = 0; i < arrData.length; i++) {
+        var row = '';
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+        row.slice(0, row.length - 1);
+        CSV += row + '\r\n';
+    }
+    if (CSV == '') {
+        alert('Invalid data');
+        return;
+    }
+    return CSV;
+}
